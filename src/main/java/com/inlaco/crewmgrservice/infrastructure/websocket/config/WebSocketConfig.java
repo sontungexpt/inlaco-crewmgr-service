8a0cf9e9bb5e5651c1f8085220e41635d3c97406
Tran Vo Son Tung <sontungexpt@gmail.com>
2026-04-19 16:35:15 +0700

    change web socket endpoint

--- src/main/java/com/inlaco/crewmgrservice/infrastructure/websocket/config/WebSocketConfig.java
+++ src/main/java/com/inlaco/crewmgrservice/infrastructure/websocket/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry
-        .addEndpoint("/ws")
+        .addEndpoint("/api/v1/ws")
         .setAllowedOriginPatterns("*") // adjust if needed security
         .withSockJS(); // optional
   }
