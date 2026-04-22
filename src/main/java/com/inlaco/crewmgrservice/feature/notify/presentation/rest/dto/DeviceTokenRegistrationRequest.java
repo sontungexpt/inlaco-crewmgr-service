44fce2310dbab8b3fe062ae854d10859f340a8b2
Tran Vo Son Tung <sontungexpt@gmail.com>
2026-04-22 21:58:29 +0700

    fix device token validation

--- src/main/java/com/inlaco/crewmgrservice/feature/notify/presentation/rest/dto/DeviceTokenRegistrationRequest.java
+++ src/main/java/com/inlaco/crewmgrservice/feature/notify/presentation/rest/dto/DeviceTokenRegistrationRequest.java
@@ -2,6 +2,7 @@ package com.inlaco.crewmgrservice.feature.notify.presentation.rest.dto;
 
 import com.inlaco.crewmgrservice.feature.notify.domain.enums.DeviceType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record DeviceTokenRegistrationRequest(
-    @NotBlank String token, @NotBlank DeviceType deviceType) {}
+    @NotBlank String token, @NotNull DeviceType deviceType) {}
